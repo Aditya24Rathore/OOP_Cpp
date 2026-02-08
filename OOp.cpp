06ed5a7ab4ccc48bbd10c6c14bdc3c4d5ccce995
Aditya Rathore <rathoreaditya262@gmail.com>
2026-02-08 11:13:18 +0530

    refactor Employee class to use constructor for initialization and add executable output

--- OOp.cpp
+++ OOp.cpp
@@ -12,12 +12,18 @@ class Employee{
         cout << "Company - " << Company << endl;
         cout << "Age - " << Age << endl;
     }
+
+    Employee(string name, string company, int age) {
+        Name = name;
+        Company = company;
+        Age = age;
+    }
 };
 
 int main() {
-    Employee employee1;
-    employee1.Name = "Aditya";
-    employee1.Company = "Self Study";
-    employee1.Age = 22;
+    Employee employee1 = Employee("Aditya", "Self Study", 22);
     employee1.Introduction();
+
+    Employee employee2 = Employee("Rishi", "Hostel", 21);
+    employee2.Introduction();
 }
