a89dffbd7662f562921c0ce5a9e807b933258ed6
Aditya Rathore <rathoreaditya262@gmail.com>
2026-02-08 21:06:21 +0530

    add exception handling example: implement basic try-catch for division by zero

---
+++ exceptionHandling.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 0;
+
+    try{
+        if(b == 0)
+              throw " division by 0 error\n";
+        int result = a/b;
+        cout << result;
+    }
+    catch(const char* e) {
+        cout << "exception caught" << e << endl;
+    }
+    return 0;
+}
