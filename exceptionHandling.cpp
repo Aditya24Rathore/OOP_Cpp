0f47c75da1fe006a4886621ffbf2f43102a84891
Aditya Rathore <rathoreaditya262@gmail.com>
2026-02-22 21:57:47 +0530

    refactor: rename variables for clarity and consistency in exception handling

--- exceptionHandling.cpp
+++ exceptionHandling.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main() {
-    int a = 10;
-    int b = 0;
+    int Number1 = 10;
+    int Number2 = 0;
 
     try{
-        if(b == 0)
-              throw " division by 0 not allowed error!!\n";
-        int result = a/b;
+        if(Number2 == 0)
+              throw " Division by 0 not allowed error!!\n";
+        int result = Number1/Number2;
         cout << result;
     }
     catch(const char* e) {
